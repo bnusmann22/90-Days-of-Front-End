7cfa1e95b58e24a94a1fcc7702793fa97b94a2c3
bnusmann22 <bn.usmann22@gmail.com>
2025-02-13 11:52:19 +0100

    More updates and toast notification

--- Sign-Up/script.js
+++ Sign-Up/script.js
@@ -1,12 +1,28 @@
-const multiColor = document.getElementById("MultiColor");
-const Colors = ["rgb(82, 22, 4)", "purple", "#7d2ae8", "green"];
+const inputField = document.querySelectorAll('input');
+const multiColor = document.getElementById('MultiColor');
+const Colors = ['rgb(82, 22, 4)', 'purple', '#7d2ae8', 'green'];
 let index = 0;
 const changeColor = () => {
   multiColor.style.color = Colors[index];
   index = (index + 1) % Colors.length;
 };
 setInterval(changeColor, 2000);
-const Data = document.querySelector("select");
+const Data = document.querySelector('select');
 const Show = () => {
   console.log(`You chose ${Data.value} Team🚀🚀`);
 };
+const showToast = (message) => {
+  let toast = document.getElementById('toast');
+  toast.innerHTML = message;
+  toast.classList.add('show');
+
+  setTimeout(() => {
+    toast.classList.remove('show');
+  }, 3000);
+};
+
+inputField.forEach((input) => {
+  input.onfocus = function () {
+    showToast(`Please enter your ${this.name}`);
+  };
+});
