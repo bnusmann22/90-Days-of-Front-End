02ba5de02fa48cdac2b62712bf6b6d9ee368af31
bnusmann22 <bn.usmann22@gmail.com>
2025-03-23 13:11:09 +0100

    more changes

--- Day12/script.js
+++ Day12/script.js
@@ -4,11 +4,14 @@ let resp = document.getElementById('res');
 let resp2 = document.getElementById('res2');
 let lName = document.getElementById('lover_name');
 let uName = document.getElementById('user_name');
+var modal = document.getElementById("myModal");
+var span = document.getElementsByClassName("close")[0];
 
 butn.addEventListener('click', function () {
   if (uName.value == '' || lName.value == '') {
     throw new Error('one or both inputs are EMPTY');
   } else {
+    modal.style.display = "block";
     let randInt = Math.floor(Math.random() * 100) + 1;
     let answer = `${randInt}% of Love Detected`;
     const checkLove =
@@ -19,28 +22,21 @@ butn.addEventListener('click', function () {
     resp.classList.remove('hidden');
     resp2.innerHTML = checkLove;
     resp.innerHTML = answer;
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
     setTimeout(() => {
       uName.value = '';
       lName.value = '';
-      resp2.innerHTML = ' ';
       resp.innerHTML = ' ';
     }, 1500);
   }
-
-  // let randInt = Math.floor(Math.random() * 100) + 1;
-  // let answer = `${randInt}% of Love Detected`;
-  // const checkLove =
-  //   randInt >= 50 ? 'Higher Love Percentage' : 'Hmm, This Love might not work';
-  // console.log(randInt);
-  // console.log(checkLove);
-  // console.log(answer);
+    
 });
 
-// const checkLove = (randInt) => {
-//   if (randInt >= 50) {
-//     console.log('Higher Love Percentage');
-//   } else {
-//    console.log('Hmm, This Love might not work');
-//  }
-// };
-// checkLove();
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
