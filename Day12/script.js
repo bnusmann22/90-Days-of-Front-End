c02f302353d300520160d60e90fc0028a35ae0db
bnusmann22 <bn.usmann22@gmail.com>
2025-04-12 00:16:58 +0100

    More on the challenge

--- Day12/script.js
+++ Day12/script.js
@@ -5,7 +5,9 @@
 let butn = document.getElementById('submitbtn');
 let resp = document.getElementById('res');
 let lName = document.getElementById('lover_name');
+let sctEl = document.getElementById('cntDwn');
 let uName = document.getElementById('user_name');
+let time = 5;
 
 butn.addEventListener('click', function (stopTimeout) {
   if (uName.value == '' || lName.value == '') {
@@ -22,6 +24,7 @@ butn.addEventListener('click', function (stopTimeout) {
     //resp2.innerHTML = checkLove;
     resp.innerHTML = answer;
     caller();
+    cntDwn();
   }
 });
 
@@ -31,10 +34,31 @@ const caller = () => {
     uName.value = '';
     lName.value = '';
     resp.innerHTML = '';
-  }, 3000);
+  }, 6700);
 };
 
 document.querySelector('.pause').addEventListener('click', () => {
-  alert('CLearance Timer Just Stopped!! ');
+  console.log('CLearance Timer Just Stopped!! ');
+  sctEl.style.contentVisibility = 'hidden';
   clearTimeout(mainCall);
+  clearTimeout(sectCall);
 });
+
+let sectCall;
+const cntDwn = () => {
+  sctEl.style.contentVisibility = 'visible';
+  sctEl.style.paddingBlock = '10px';
+  sctEl.innerHTML = `Inputs would clear in: <b >${time}</b>
+  <br/>
+  <em>Tap to stop clearance</em>
+  `;
+  time--;
+
+  if (time >= 1) {
+    sectCall = setTimeout(cntDwn, 1650);
+  } else {
+    sctEl.style.contentVisibility = 'hidden';
+    console.log('CLEARED!!');
+    location.reload()
+  }
+};
--- Day12/test.js
+++ Day12/test.js
@@ -1,11 +1,30 @@
 let butn = document.getElementById('submitbtn');
 let time = 5;
 butn.onclick = () => {
-  console.log( setInterval(() => {
-    time - 1;
-  }, 1000););
+  cntDwn();
 };
 
-const cntDwn = () =>{
- 
-}
+let sectCall;
+let sctEl = document.getElementById('cntDwn');
+const cntDwn = () => {
+  sctEl.style.contentVisibility = 'visible';
+  sctEl.style.paddingBlock = '10px';
+  sctEl.innerHTML = `Inputs would clear in: <b >${time}</b>
+  <br/>
+  <em>Tap to stop clearance</em>
+  `;
+  time--;
+
+  if (time >= 1) {
+    sectCall = setTimeout(cntDwn, 1650);
+  } else {
+    sctEl.style.contentVisibility = 'hidden';
+    console.log('Go!!');
+  }
+};
+
+document.querySelector('.pause').addEventListener('click', () => {
+  console.log('CLearance Timer Just Stopped!! ');
+  sctEl.style.contentVisibility = 'hidden';
+  clearTimeout(sectCall);
+});
